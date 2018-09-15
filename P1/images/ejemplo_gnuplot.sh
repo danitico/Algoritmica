3522f7adc10c571e91ff06d2852b3e2cc2b36d17
Daniel Ranchal <danitico98@gmail.com>
2018-09-15 11:18:48 +0200

    :)

--- P1/images/ejemplo_gnuplot.sh
+++ P1/images/ejemplo_gnuplot.sh
@@ -6,5 +6,5 @@ set output "grafico.eps"
 set key right bottom
 set xlabel "Número de elementos"
 set ylabel "Tiempo en microsegundos"
-plot 'Datos.txt' using 1:2 t "Datos columna 2 frente a 1" w l
+plot 'Datos1.txt' using 1:2 t "Datos columna 2 frente a 1" w l
 _end_
