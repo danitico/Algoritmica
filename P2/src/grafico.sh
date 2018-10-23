f1f9aaa7d8eb60c6968a607774c7fecbb5825be0
Daniel Ranchal <danitico98@gmail.com>
2018-10-23 17:58:32 +0200

    I updated the script. Now it accepts an argument with the name of the file. I updated the files with the times of each algorithm and the graphics.

--- P2/src/grafico.sh
+++ P2/src/grafico.sh
@@ -6,5 +6,5 @@ set output "$1.eps"
 set key right bottom
 set xlabel "Numero de elementos"
 set ylabel "Tiempo en microsegundos"
-plot "$1" using 1:2 t "Real" w l, "$1" using 1:3 t "Estimado" w l
+plot "$1.txt" using 1:2 t "Real" w l, "$1.txt" using 1:3 t "Estimado" w l
 _end_
