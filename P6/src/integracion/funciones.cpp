edcc7744f4fb42ee1653f48eb93248a06d251c9a
Daniel Ranchal <danitico98@gmail.com>
2018-12-08 13:34:12 +0100

    Práctica terminada (?)

--- P6/src/integracion/funciones.cpp
+++ P6/src/integracion/funciones.cpp
@@ -2,21 +2,24 @@
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
-double integracionNumerica(long long int simulaciones, long long int a, long long int b){
+double integracionNumerica(int simulaciones, int a, int b){
    double suma=0.0;
-   long long int x=0;
+   int x=0;
    for(int i=0; i<simulaciones; i++){
       x=rand()%(b - a + 1) + a;
-      suma += exp(pow(x, 2)*(-0.5)) / sqrt(2*M_PI);
+      suma += function(x);
    }
 
    return ((b-a)*suma) / simulaciones;
 }
 void problemaIntegracion(){
-   long long int simulaciones, a, b;
+   int simulaciones, a, b, intervalos;
 
    std::cout << BIBLUE << "Introduzca cuantas simulaciones quiere realizar: " << RESET;
    std::cin >> simulaciones;
+
+   std::cout << BIBLUE << "Introduzca intervalos: " << RESET;
+   std::cin >> intervalos;
 
    std::cout << BIBLUE << "Introduzca el límite inferior: " << RESET;
    std::cin >> a;
@@ -24,6 +27,27 @@ void problemaIntegracion(){
    std::cout << BIBLUE << "Introduzca el límite superior: " << RESET;
    std::cin >> b;
 
-   std::cout << "El valor de la integral es " << integracionNumerica(simulaciones, a, b) << std::endl;
+   std::cout << BIPURPLE << "El valor de la integral es " << RESET
+             << UNDERLINE << BIRED
+             << integracionNumerica(simulaciones, a, b) << RESET << std::endl;
+
+   std::cout << BIPURPLE << "El valor de la integral con trapecios es "
+             << RESET << UNDERLINE << BIRED
+             << metodoTrapecios(a, b, intervalos) << RESET << std::endl;
+
    std::cin.ignore();
+}
+double metodoTrapecios(int a, int b, int intervalos){
+   double h = double(b-a) / double(intervalos);
+   double suma=0.0;
+
+   suma = function(a) + function(b);
+   for(int i=1; i<=intervalos; i++){
+      suma += 2.0 * function(a + h*i);
+   }
+
+   return (suma * h) / 2;
+}
+double function(double x){
+   return exp(pow(x, 2)*(-0.5)) / sqrt(2*M_PI);
 }
--- P6/src/integracion/funciones.hpp
+++ P6/src/integracion/funciones.hpp
@@ -1,7 +1,8 @@
 #ifndef FUNCIONES_HPP
 #define FUNCIONES_HPP
 #include "../macros.hpp"
-void metodoTrapecios();
+double metodoTrapecios(int a, int b, int intervalos);
+double function(double x);
 double integracionNumerica(long long int simulaciones, long long int a, long long int b);
 void problemaIntegracion();
 #endif
